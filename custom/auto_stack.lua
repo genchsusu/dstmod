7e4fcf9a1159b765bd049cb4aa8a084647064367
genchsusu <pctxiaozhu@gmail.com>
2023-12-16 10:11:21 +0800

    feat: now smallcreature or flying can't be autostacked

--- custom/auto_stack.lua
+++ custom/auto_stack.lua
@@ -8,12 +8,20 @@ end
 local autoStackEnabled = true
 local autoStackRange = 10
 
+local function CanAutoStack(item)
+    if not item:HasTag("smallcreature") and not item:HasTag("flying") then
+        return true
+    end
+    return false
+end
+
 local function TryAddToStack(item, target)
     if item and target and target ~= item and target.prefab == item.prefab
     and item.components.inventoryitem and item.components.inventoryitem.owner == nil
     and target.components.inventoryitem and target.components.inventoryitem.owner == nil
     and target.components.stackable and not target.components.stackable:IsFull()
-    and item.components.stackable and not item.components.stackable:IsFull() then
+    and item.components.stackable and not item.components.stackable:IsFull()
+    and CanAutoStack(item) then
         target.components.stackable:Put(item)
         return true
     end
@@ -22,7 +30,8 @@ end
 
 local function AutoStack(inst)
     if inst:IsValid() and inst.components.stackable and inst.components.inventoryitem
-    and inst.components.inventoryitem.owner == nil and not inst.components.stackable:IsFull() then
+    and inst.components.inventoryitem.owner == nil and not inst.components.stackable:IsFull()
+    and CanAutoStack(inst) then
         local x, y, z = inst.Transform:GetWorldPosition()
         local ents = TheSim:FindEntities(x, y, z, autoStackRange, { "_inventoryitem" }, { "INLIMBO", "NOCLICK", "catchable", "fire" })
         for _, obj in pairs(ents) do
