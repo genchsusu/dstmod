70445d30d261c77b7653b3568808641a8057913b
genchsusu <pctxiaozhu@gmail.com>
2023-12-17 16:16:48 +0800

    feat: add code

--- modinfo.lua
+++ modinfo.lua
@@ -3,119 +3,27 @@ author = "OpenSource"
 version = "0.1"
 
 description = [[
-    个人mod合集
+个人mod合集:
 
-    components
-    ## birdspawner 驱逐鸟类
-    --用的时候增加标签
-    inst:AddTag("drive_bird_scarecrow")
-    如main/watertree_pillar.lua
-    ## boatphysics
-    船只最大转向速度
-    ## firedetector
-    firedetector组建不侦测campfire的tag
-    ## nosnow
-    下雪不堆积
-    ## respawn
-    死亡不掉落物品 重生15秒无敌
-    ## protectionwall
-    保护圈 360度保护
-
-    
-    brain
-    ## perdbrain
-    火鸡晚上不回窝
-
-    
-    prefabs
-    ## all_backpacks
-    krampus_sack,piggyback,backpack背包冰箱,防水,发光,补脑,体温恒定35度
-    ## all_chests
-    "dragonflychest", "fish_box", "icebox", "magician_chest", "saltbox", "treasurechest", "shadowchester", "shadow_container", "minotaurchest"
-    扩大箱子,冰箱,防水,无法被摧毁
-    其中
-    treasurechest增加了吸尘器系统
-    ## all_season_farm_plant
-    四季都可以种植,必然超大果,农田采摘完毕没有荆棘然后生成耕地然后自动种植,dig_up/burnt自动生成耕地
-    ## bananabush
-    植物不枯萎,高速收获,永远可摘
-    ## beebox
-    蜂箱提高至999,收获不出蜜蜂
-    ## boat
-    boat/boat_grass永不沉没and增加吸鱼功能,猴子船降到1滴血
-    ## fire
-    "campfire", "firepit", "coldfire", "coldfirepit"
-    调用protectionwall 生成保护墙
-    ## firesuppressor
-    庇护所 防鸟 防蚁狮 避雷针
-    ## force_plant
-    可以种植芦苇,花瓣
-    ## friendlyfruitfly 友善果蝇
-    不会被冰冻
-    不会被点燃
-    最大血量1000且高速回血
-    远离玩家时依然会工作
-    ## general_plants
-    植物提高产量    
-    grass,berrybush,berrybush2,berrybush_juicy,monkeytail,sapling,sapling_moon
-    ## infiniteuses
-    无限耐久
-    ## log
-    烧木头生成灰10 木炭10
-    ## perd
-    火鸡生蛋
-    ## pond
-    池塘边生成曼德拉草
-    ## quick_work
-    高效工作,采集,砍树,挖矿,锤,淡水急速钓鱼
-    ## rock_avocado_bush
-    石果快速生成
-    ## rocks
-    石矿无限开采,提升产量
-    ## soil_amender
-    化肥可堆叠
-    ## stack_ui_fix
-    修复堆叠数量显示不正确的bug
-    ## walls
-    墙吸收100%伤害,无法被瞬间摧毁,防鸟,防蚁狮,避雷针,发光,补脑,玩家可以直接穿透
-    ## watertree_pillar
-    水中木驱鸟
-    ## fire_wall
-    火焰形状的墙 无热量 仅玩家可以通过
-
-    
-    Utils
-    ## auto_stack
-    自动堆叠物品
-    ## containers
-    容器扩大80和160
-    ## resurrect
-    Y/U根据游戏语言输入#复活/#resurrect 进行复活
-    ## standardcomponents
-    冬天继续生长
-    ## tuning
-    堆叠999
-    复活无惩罚
-    无草蜥蜴
-    高速挖田
-    火鸡走路慢
-    走路手杖2倍移速
-    消耗品几乎无消耗
-
-    
-    TODO 
-    魔术箱子仍然没有动画
-    evergreens
-    farm_plants
-    farm_plow
-    weed_plants
-    ]]
+船只功能:增强转向速度,永不沉没,吸鱼功能,猴子船hp=1
+灭火与保护:灭火器不灭营火,庇护所功能,防蚁狮,避雷针
+天气与季节:雪不积累,冬季作物生长,营火生成无热量火墙,只允许玩家通过
+生存加强:无惩罚复活,死亡后输入#复活,15秒重生无敌,火鸡晚上不回窝且生蛋
+农业改良:四季可种植,自动种植与耕地生成,植物快速成熟,不枯萎,提高产量
+特殊物品:蜂箱存量增至999,无蜜蜂出现,可种植特殊植物如芦苇和花瓣,池塘边长曼德拉草
+友善果蝇:不会冰冻或燃烧,高血量与快速回血
+工具与资源:工具无限耐久,高效工作模式,资源如石矿无限开采
+自动化与堆叠:物品自动堆叠,最大堆叠量999,化肥可堆叠,修复堆叠bug
+防御系统:墙体坚固,具备多功能防御,玩家可穿透
+其他:水中木驱鸟,无草蜥蜴,加速农田挖掘,火鸡行走缓慢,手杖移速加倍,消耗品耐久提升
+背包与箱子:全背包具备冰箱功能,防水,发光,增强大脑,保持体温,箱子容量增大,防摧毁,儲物箱添加吸尘器功能
+]]
 
 forumthread = ""
 
 api_version = 10
 
-icon_atlas = "modicon.xml"
+icon_atlas = "images/modicon.xml"
 icon = "modicon.tex"
 
 dst_compatible = true
